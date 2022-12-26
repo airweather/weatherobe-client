35d824dd16f8eed9ca9b132ddefc74786caec85d
Jun <wnsghd1@naver.com>
2022-12-27 05:41:22 +0900

    invalid host header 문제 수정

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
-const target = process.env.TARGET.ADDRESS;
+const target = process.env.VUE_APP_TARGET_ADDRESS;
 
 module.exports = {
   devServer: {
+    disableHostCheck: true,
     port:8080,
     proxy: {
       '^/api': {
