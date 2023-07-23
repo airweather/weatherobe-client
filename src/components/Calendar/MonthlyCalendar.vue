278bd5b25cdde9472a3a4db9b279733b4f1df82f
Jun <wnsghd1@naver.com>
2023-07-23 23:32:35 +0900

    Calendar frame

--- src/components/Calendar/MonthlyCalendar.vue
+++ src/components/Calendar/MonthlyCalendar.vue
@@ -1,7 +1,40 @@
 <template>
   <div>
     <h1>weather calendar</h1>
+    
+    <div class="calendar-container">
 
+      <div class="calendar-header">
+
+        <div class="calendar-header-button" @click="moveToPrevMonth">&lt;</div>
+        
+        <div>
+          {{ anchorDate.getFullYear() }}년 {{ anchorDate.getMonth() + 1 }}월
+        </div>
+        <div class="calendar-header-button" @click="moveToNextMonth">></div>
+      </div>
+
+      <div class="calendar-body">
+
+        <div v-for="(item, i) in days" :key="i" class="days">
+          <div>{{ item }}</div>
+        </div>
+  
+        <div v-for="(item, i) in calendar" :key="i">
+          <div>
+            {{ item?.date.getDate()}}
+          </div>
+          <div>
+            {{ item?.weatherInfo  }}
+          </div>
+  
+        </div>
+      </div>
+
+      
+
+    </div>
+    
     
     
 
@@ -18,15 +51,92 @@ export default {
 // 과거의 날씨는 컬러를 회색으로 표시
 // 날씨 api를 활용해서 온도, 특이기상 표시
 
-
+created() {
+  this.makeCalendar();
+  this.hello();
+},
 data() {
   return {
-    test:'test'
+    test:'weather calendar',
+    calendar: [],
+    anchorDate: new Date(),
+    days: ['일', '월', '화', '수', '목', '금', '토'],
+    
   }
 },
+
+methods: {
+  renderDate(date) {
+    return date.getDate();
+  },
+  moveToNextMonth() {
+    this.anchorDate = new Date(this.anchorDate.getFullYear(), this.anchorDate.getMonth() + 1, 1);
+    this.makeCalendar();
+  },
+  
+  moveToPrevMonth() {
+    this.anchorDate = new Date(this.anchorDate.getFullYear(), this.anchorDate.getMonth() - 1, 1);
+    this.makeCalendar();
+  },
+  makeCalendar() {
+
+    const firstDay = new Date(this.anchorDate.getFullYear(), this.anchorDate.getMonth(), 1);
+    const firstDayOfMonth = firstDay.getDay();
+
+
+    const calendar = Array.from({length: 42}, (_, i) => 
+      {
+        if( i < firstDayOfMonth || i >= firstDayOfMonth + new Date(this.anchorDate.getFullYear(), this.anchorDate.getMonth() + 1, 0).getDate()) {
+          return;
+        }
+
+        const date = new Date(firstDay.getFullYear(), firstDay.getMonth(), i - firstDayOfMonth+1);
+
+        const dateObject = {
+          date,
+          weatherInfo : "",
+          lowestTemp : 0,
+          highestTemp : 0,
+        }
+        
+        return dateObject;
+      });
+
+
+    this.calendar = calendar;
+
+  },
+
+  hello() {
+    console.log(this.calendar);
+  }
+}
 }
 </script>
 
 <style>
+.calendar-container{
+}
+
+.calendar-body{
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+
+}
+
+.calendar-header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.calendar-header-button{
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
 
 </style>
